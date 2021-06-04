ef9cbec10456795034bc5a8bb263c57acc3e1143
Jo Riviera <guirito.jc@gmail.com>
2021-06-04 16:44:39 +0200

    entry point configured, with logger

---
+++ app.ts
@@ -0,0 +1,53 @@
+import expresss from 'express';
+import * as http from "http";
+
+import * as winston from "winston";
+import * as expressWinston from "express-winston";
+import cors from "cors";
+import { CommonRoutesConfig } from './common/common.routes.config';
+import { UsersRoutes } from './users/users.routes.config';
+import debug from 'debug';
+import express from 'express';
+
+const app: express.Application = express();
+const server: http.Server = http.createServer(app);
+const port = 3000;
+const routes: Array<CommonRoutesConfig> = [];
+const debugLog: debug.IDebugger = debug('app');
+
+// Adding Middleware to the Express app
+// Parse requests as JSON
+app.use(express.json());
+// Add Cross Origin Request treatment
+app.use(cors());
+
+//logging http requests
+const loggerOptions: expressWinston.LoggerOptions = {
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.json(),
+    winston.format.prettyPrint(),
+    winston.format.colorize({ all: true })
+  ),
+};
+
+if (!process.env.DEBUG) {
+  loggerOptions.meta = false; // log reqs as one-liners when not debugging
+}
+
+app.use(expressWinston.logger(loggerOptions));
+// has to be after winston setup
+routes.push(new UsersRoutes(app));
+
+const runningMessage = `Server running at hhtp://localhost:${port}`;
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.status(200).send(runningMessage);
+})
+
+server.listen(port, () => {
+  routes.forEach((route: CommonRoutesConfig) => {
+    debugLog(`Routes configured for ${route.getName()}`);
+  });
+
+  console.log(runningMessage);
+})

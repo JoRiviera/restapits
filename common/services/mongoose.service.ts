a7756099b032d8c73d8222f2e4e51b6e1f763f38
Jo Riviera <guirito.jc@gmail.com>
2021-06-11 19:24:31 +0200

    Impl. MongooseService

---
+++ common/services/mongoose.service.ts
@@ -0,0 +1,42 @@
+import mongoose, { Mongoose } from 'mongoose';
+import debug from 'debug';
+
+const log: debug.IDebugger = debug('app:mongoose-service');
+
+class MongooseService {
+  private count = 0;
+  private mongooseOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectedTimeoutMS: 5000,
+    useFindAndModify: false,
+  }
+
+  constructor() {
+    this.connectWithRetry();
+  }
+
+  connectWithRetry() {
+    log("Attempting MongoDB connection (will retry if needed)'");
+    mongoose
+      .connect('mongodb:localhost:27017/api-db', this.mongooseOptions)
+      .then(() => {
+        log("MongoDB is connected.");
+      })
+      .catch((err) => {
+        const retrySeconds = 5;
+        log(
+          `MongoDB connection unsuccessful (will retry #${++this.count} after ${retrySeconds}:`,
+          err
+        );
+        setTimeout(this.connectWithRetry, retrySeconds * 1000);
+      });
+  }
+
+  getMongoose() {
+    return mongoose;
+  }
+
+}
+
+export default new MongooseService();
